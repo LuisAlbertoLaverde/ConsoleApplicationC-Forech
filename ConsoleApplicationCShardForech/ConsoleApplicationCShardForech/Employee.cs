a654126232d89b060bf74d86f4fc57ee0ce8720a
Alberto <unumnio@gmail.com>
2022-11-16 20:23:13 -0600

    using foreach method

---
+++ ConsoleApplicationCShardForech/ConsoleApplicationCShardForech/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationCShardForech
+{
+    internal class Employee
+    {
+        //Atributos privados de la clase, siempre se recomiendan que sean privados
+        private string _IDEmployee;
+        private string _Name;
+        private string _LastName;
+        private long _Salary;
+        private DateTime _ContractDate;
+
+        //Constructores son siempre public
+        public Employee(string iDEmployee, string name, string lastName, long salary, DateTime contractDate)
+        {
+            _IDEmployee = iDEmployee;
+            _Name = name;
+            _LastName = lastName;
+            _Salary = salary;
+            _ContractDate = contractDate;
+        }
+        public Employee()
+        {
+        }
+
+        //Propiedades
+        public string IDEmployee
+        {
+            get { return _IDEmployee; } set { _IDEmployee = value; } 
+        }
+        public string Name
+        {
+            get { return _Name; } set { _Name = value; }
+        }
+        public string LastName
+        {
+            get { return _LastName; } set { _LastName = value; }
+        }
+        public long Salary
+        {
+            get { return _Salary; } set { _Salary = value; }
+        }
+        public DateTime ContractDate
+        {
+            get { return _ContractDate; } set { _ContractDate = value; }
+        }
+
+        //Metodos
+        public override string ToString()
+        {
+            return "ID Employee" + IDEmployee + "\n" 
+                + "\tNombres...................." + _Name + "\n"
+                + "\tApellidos.................." + _LastName + "\n"
+                + "\tSalario...................." + _Salary + "\n"
+                + "\tFecha de contratacion......" + _ContractDate + "\n";
+        }
+    }
+}
---
+++ ConsoleApplicationCShardForech/ConsoleApplicationCShardForech/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApplicationCShardForech;
+
+
+Employee Employee1 = new Employee("1010", "Sara", "Laverde", 8100000, new DateTime(2022, 3, 25));
+Employee Employee2 = new Employee("1020", "Albert", "Laverde", 8200000, new DateTime(2020, 8, 23));
+Employee Employee3 = new Employee("1030", "Cristhian", "Laverde", 8300000, new DateTime(2021, 6, 15));
+Employee Employee4 = new Employee("1040", "Andres", "Castro", 8400000, new DateTime(2020, 4, 25));
+Employee Employee5 = new Employee("1050", "ADA", "Piedrahita", 8500000, new DateTime(2023, 7, 05));
+Employee Employee6 = new Employee();
+
+Employee6.IDEmployee = "1060";
+Employee6.Name = "Jorge";
+Employee6.LastName = "Martinez";
+Employee6.Salary = 8600000;
+Employee6.ContractDate = new DateTime(2024, 12, 31);
+
+Employee[] employees = new Employee[6];
+employees[0] = Employee1;
+employees[1] = Employee2;
+employees[2] = Employee3;
+employees[3] = Employee4;
+employees[4] = Employee5;
+employees[5] = Employee6;
+
+foreach (var employee in employees)
+{
+    Console.WriteLine(employee.ToString());
+}
